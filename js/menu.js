64aafd89cefd536c81ffd490181e70cfe0fb1c6b
M0rrighan <81151384+M0rrighan@users.noreply.github.com>
2021-08-30 20:15:49 +0200

    Fix linter errors, Update linter config files

--- js/menu.js
+++ js/menu.js
@@ -2,15 +2,15 @@ const body = document.getElementsByTagName('body')[0];
 const menu = document.querySelector('header nav ul.menu');
 const openMenuBtn = document.querySelector('.openMenuBtn');
 const closeMenuBtn = document.querySelector('.closeMenuBtn');
-const navLinks = document.querySelectorAll('header nav ul.menu li')
+const navLinks = document.querySelectorAll('header nav ul.menu li');
 
-function showMenu () {
+function showMenu() {
   menu.classList.add('opened');
   closeMenuBtn.style.display = 'block';
   body.style.overflow = 'hidden';
 }
 
-function hideMenu () {
+function hideMenu() {
   menu.classList.remove('opened');
   closeMenuBtn.style.display = 'none';
   body.style.overflow = 'auto';
@@ -19,6 +19,6 @@ function hideMenu () {
 openMenuBtn.addEventListener('click', showMenu);
 closeMenuBtn.addEventListener('click', hideMenu);
 
-navLinks.forEach(link => {
+navLinks.forEach((link) => {
   link.addEventListener('click', hideMenu);
 });
