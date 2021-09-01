70a451b92db033ee064cc0a4caa1b9fb2827de12
M0rrighan <81151384+M0rrighan@users.noreply.github.com>
2021-09-01 10:40:51 +0200

    Create insertion.js file, Add const = Array of objects

---
+++ js/insertions.js
@@ -0,0 +1,106 @@
+const projects = [
+  {
+    title: 'Tonic',
+    details: {
+      client: 'Canopy',
+      role: 'Back End Dev',
+      year: '2015',
+    },
+    shortDescription: `A daily selection of privately personalized reads; no accounts or 
+    sign-ups required.`,
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
+    unknown printer took a galley of type and scrambled it to make a type specimen book. 
+    It has survived not only five centuries, but also the leap into electronic typesetting, 
+    remaining essent, Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
+    unknown printer took a galley of type and scrambled it to make a type specimen book. 
+    It has survived not only five centuries, but also the leap into electronic typesetting, 
+    remaining essent`,
+    image: {
+      imageUrl: './img/snapshoot1.png',
+      imageAlt: 'project preview image',
+    },
+    technologies: ['html', 'css', 'javaScript'],
+    liveUrl: 'TonicLive.html',
+    soureUrl: 'Tonic.html',
+  },
+  {
+    title: 'Multi-Post Stories',
+    details: {
+      client: 'Facebook',
+      role: 'Full Stack Dev',
+      year: '2015',
+    },
+    shortDescription: `Experimental content creation feature that allows users to add to an 
+    existing story over the course of a day without spamming their friends.`,
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
+    unknown printer took a galley of type and scrambled it to make a type specimen book. 
+    It has survived not only five centuries, but also the leap into electronic typesetting, 
+    remaining essent, Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
+    unknown printer took a galley of type and scrambled it to make a type specimen book. 
+    It has survived not only five centuries, but also the leap into electronic typesetting, 
+    remaining essent`,
+    image: {
+      imageUrl: './img/snapshoot2.png',
+      imageAlt: 'project preview image',
+    },
+    technologies: ['html', 'css', 'javaScript'],
+    liveUrl: 'mpStoriesLive.html',
+    soureUrl: 'mpStories.html',
+  },
+  {
+    title: 'Facebook 360',
+    details: {
+      client: 'Facebook',
+      role: 'Full Stack Developer',
+      year: '2015',
+    },
+    shortDescription: `Exploring the future of media in Facebook's first Virtual Reality app; 
+    a place to discover and enjoy 360 photos and videos on Gear VR.`,
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
+    unknown printer took a galley of type and scrambled it to make a type specimen book. 
+    It has survived not only five centuries, but also the leap into electronic typesetting, 
+    remaining essent, Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
+    unknown printer took a galley of type and scrambled it to make a type specimen book. 
+    It has survived not only five centuries, but also the leap into electronic typesetting, 
+    remaining essent`,
+    image: {
+      imageUrl: './img/snapshoot3.png',
+      imageAlt: 'project preview image',
+    },
+    technologies: ['html', 'css', 'javaScript'],
+    liveUrl: 'TonicLive.html',
+    soureUrl: 'Tonic.html',
+  },
+  {
+    title: 'Uber Navigation',
+    details: {
+      client: 'Uber',
+      role: 'Lead Developer',
+      year: '2018',
+    },
+    shortDescription: `A smart assistant to make driving more safe, efficient, 
+    and fun by unlocking your most expensive computer: your car.`,
+    description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
+    unknown printer took a galley of type and scrambled it to make a type specimen book. 
+    It has survived not only five centuries, but also the leap into electronic typesetting, 
+    remaining essent, Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+    Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an 
+    unknown printer took a galley of type and scrambled it to make a type specimen book. 
+    It has survived not only five centuries, but also the leap into electronic typesetting, 
+    remaining essent`,
+    image: {
+      imageUrl: './img/snapshoot4.png',
+      imageAlt: 'project preview image',
+    },
+    technologies: ['html', 'css', 'javaScript'],
+    liveUrl: 'mpStoriesLive.html',
+    soureUrl: 'mpStories.html',
+  },
+];
