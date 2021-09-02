2a4e60eb143b70c309d8edc46e488e1c8e44988e
Ben Mukebo <l.benkasmukebo7@gmail.com>
2021-09-02 13:35:40 +0200

    Add some style to the alert message from the form js

--- js/form.js
+++ js/form.js
@@ -1,15 +1,19 @@
 const form = document.getElementById('form');
 const email = document.getElementById('email');
 const alertParent = document.querySelector('form ul li:last-child');
-const alertMsg = document.createElement('small');
+const alertMsg = document.createElement('p');
 alertParent.appendChild(alertMsg);
 alertMsg.style.visibility = 'hidden';
 alertMsg.textContent = ' * Please change your email to lower case.';
+alertMsg.style.color = '#e74c3c';
+alertMsg.style.paddingTop = '7px';
+alertMsg.style.fontWeight = 'bold';
+
 
 form.addEventListener('submit', (e) => {
   if (email.value !== email.value.toLowerCase()) {
     alertMsg.style.visibility = 'visible';
-    email.style.border = '1px solid #e74c3c';
+    email.style.border = '2px solid #e74c3c';
     e.preventDefault();
   } else {
     alertMsg.style.visibility = 'hidden';
