c483d6bb85459477a401814b14ad99fbe8cc0ac3
M0rrighan <81151384+M0rrighan@users.noreply.github.com>
2021-09-02 13:20:50 +0200

    Add event listener on form submit, checks valid email format

--- js/form.js
+++ js/form.js
@@ -3,4 +3,16 @@ const email = document.getElementById('email');
 const alertParent = document.querySelector('form ul li:last-child');
 const alertMsg = document.createElement('small');
 alertParent.appendChild(alertMsg);
+alertMsg.style.visibility = 'hidden';
 alertMsg.textContent = ' * Please change your email to lower case.';
+
+form.addEventListener('submit', (e) => {
+  if (email.value !== email.value.toLowerCase()) {
+    alertMsg.style.visibility = 'visible';
+    email.style.border = '1px solid #e74c3c';
+    e.preventDefault();
+  } else {
+    alertMsg.style.visibility = 'hidden';
+    email.style.border = '1px solid #d0d9d4';
+  }
+});
